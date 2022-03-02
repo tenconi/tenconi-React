1879e6631ce208a9079a13fd103a7a0937d49d8a
tenconi <47565305+tenconi@users.noreply.github.com>
2022-03-02 14:04:52 +0100

    condicional para empty cart

---
+++ src/components/EmptyCart/EmptyCart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './EmptyCart.css'
+
+const EmptyCart = () => {
+  return (
+    <div className='positionEmpty'>
+        <h2 className=''>Aún no has seleccionado productos.</h2>
+    </div>
+  )
+}
+
+export default EmptyCart
--- src/views/Cart.jsx
+++ src/views/Cart.jsx
@@ -1,5 +1,6 @@
 import React, {useContext, useState} from 'react';
 import { CartContext } from '../context/cartContext';
+import EmptyCart from '../components/EmptyCart/EmptyCart.jsx';
 import './Styles.css'
 
 
@@ -13,11 +14,11 @@ const{cart, useCart, removeItem, clearCart}= useContext(CartContext)
   return <div className="altura">
       <div className='contenedor'> 
 
-
+      {cart.length === 0 ? <EmptyCart/> : <div>
             {cart.map((PR, initial)=>{            
               return(
                 <>
-                  <div className="cartProduct" key={PR.id}>                
+                  <div className="cartProduct" key={cart.id}>                
                     <img src={PR.img} className="cartProduct__img" alt={PR.prod}/>
 
                     <table border="0" cellSpacing={0} className="cartProduct__table" >
@@ -33,15 +34,17 @@ const{cart, useCart, removeItem, clearCart}= useContext(CartContext)
                         <td><h4 className="itemCard__detail--price">$  {PR.price * PR.cantidad}</h4></td>
                       </tr>                  
                     </table>
-
                     
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" className='cartProduct__close' onClick={()=> {removeItem(PR.id)}}><path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"/></svg>
                     </div>
                 </>
               )
             })}
-              <button onClick={()=>{clearCart(cart)}}>BORRAR CARRo</button>
+            <button onClick={()=>{clearCart(cart)}} className="btn__clear">Vaciar Carrito</button>
             <h3>Total: </h3>
+            </div>}
+
+            
 
       </div>
     </div>
